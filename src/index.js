24800c4e60adf571a467572caac521e40782a067
Kagan Inan <contact@kaaninan.net>
2022-09-13 23:36:06 +0300

    fixes #9

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const path = require('path')
+const fs = require('fs');
 const PORT = 8001;
 
 app.use(cors())
 app.use('/static', express.static(path.join(__dirname, 'public')))
+app.get('/list', (req, res) => {
+	let list = []
+	fs.readdir(path.join(__dirname, 'tileserver/tilesets'), (err, files) => {
+		files.forEach(file => {
+			if(file != '.DS_Store' && file != 'README.md'){
+				list.push(file)
+			}
+		});
+		res.send(list)
+	});
+});
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
